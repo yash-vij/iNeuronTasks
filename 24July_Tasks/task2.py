dcce14bdc512a78daae60fa4bd8b4d7656f907d1
yash-vij <yashvij1996@gmail.com>
2022-07-30 13:40:29 +0530

    Task1 : Create a  table attribute dataset and dress dataset

---
+++ 24July_Tasks/task2.py
@@ -0,0 +1,7 @@
+# Do a bulk load for these two table for respective dataset
+import pandas as pd
+read_file= pd.read_excel("C:\ineuron\Attribute DataSet.xlsx")
+print(read_file)
+
+store_in_csv = pd.to_csv()
+
