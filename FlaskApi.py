55a08df6ebec30126b5e8cc926422350927f6f72
yash-vij <yashvij1996@gmail.com>
2022-08-22 14:29:48 +0530

    Flask API

--- FlaskApi.py
+++ FlaskApi.py
@@ -5,24 +5,37 @@ app = Flask(__name__)
 
 mydb = conn.connect(host="localhost", user='root', passwd="0000")
 cursor = mydb.cursor()
-cursor.execute("create database if not exists taskdb")
-cursor.execute("create table if not exists taskdb.tasktable (name varchar(30) , number int)")
+cursor.execute("create database if not exists APItest")
+cursor.execute("create table if not exists APItest.table1 (id int, name varchar(30))")
 
 
 @app.route('/insert', methods=['POST'])
 def insert():
     if request.method == 'POST':
+        id = request.json['id']
         name = request.json['name']
-        number = request.json['number']
-        cursor.execute("insert into taskdb.tasktable  values(%s , %s)", (name, number))
+        cursor.execute("insert into APItest.table1 values(%s , %s)", (id, name))
+        mydb.commit()
+        return jsonify(str('succesfully inserted'))
+
+@app.route('/update', methods=['POST'])
+def update():
+    if request.method == 'POST':
+        id = request.json['id']
+        name = request.json['name']
+        cursor.execute("update APItest.table1 set name = (%s) where id = (%s) ",(name,id))
         mydb.commit()
         return jsonify(str('succesfully inserted'))
 
 
+@app.route('/delete', methods=['POST'])
+def delete():
+    if request.method == 'POST':
+        id = request.json['id']
+        cursor.execute("delete from APItest.table1 where id = (%s)", (id,))
+        mydb.commit()
+        return jsonify(str('succesfully deleted'))
+
 if __name__ == '__main__':
     app.run()
 
-    {
-        "name": "nizam",
-        "number": 6867
-    }
