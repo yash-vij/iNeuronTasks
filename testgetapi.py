42bfd93679de1833f79f4108fab6477ae9eb040a
yash-vij <yashvij1996@gmail.com>
2022-08-24 21:51:18 +0530

    Flask API

--- testgetapi.py
+++ testgetapi.py
@@ -5,7 +5,9 @@ app = Flask(__name__)
 @app.route('/testapi')
 def test():
     get_name = request.args.get("get_name")
-    return "Testing GET API {}".format(get_name)
+    mobile_number = request.args.get("mobile_number")
+
+    return "Testing GET API {} {}".format(get_name, mobile_number)
 
 if __name__ == "__main__":
     app.run(port=5003)
